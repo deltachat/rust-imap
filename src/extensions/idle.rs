0f90f4c3b804860cebcf4745c0493ce778cf3988
dignifiedquire <me@dignifiedquire.com>
2019-10-24 20:35:49 +0200

    restore read_timeout

--- src/extensions/idle.rs
+++ src/extensions/idle.rs
@@ -29,6 +29,7 @@ pub struct Handle<'a, T: Read + Write> {
     session: &'a mut Session<T>,
     keepalive: Duration,
     done: bool,
+    old_timeout: Option<Duration>,
 }
 
 /// Must be implemented for a transport in order for a `Session` using that transport to support
@@ -43,6 +44,9 @@ pub trait SetReadTimeout {
     ///
     /// See also `std::net::TcpStream::set_read_timeout`.
     fn set_read_timeout(&mut self, timeout: Option<Duration>) -> Result<()>;
+
+    /// Returns the read timeout of this socket.
+    fn read_timeout(&self) -> Result<Option<Duration>>;
 }
 
 impl<'a, T: Read + Write + 'a> Handle<'a, T> {
@@ -51,6 +55,7 @@ impl<'a, T: Read + Write + 'a> Handle<'a, T> {
             session,
             keepalive: Duration::from_secs(29 * 60),
             done: false,
+            old_timeout: None,
         };
         h.init()?;
         Ok(h)
@@ -97,7 +102,7 @@ impl<'a, T: Read + Write + 'a> Handle<'a, T> {
     /// that the underlying stream was closed and a reconnect is neccessary
     fn wait_inner(&mut self) -> Result<bool> {
         let mut v = Vec::new();
-        match self.session.readline(&mut v).map(|_| ()) {
+        match self.session.readline(&mut v) {
             Err(Error::Io(ref e))
                 if e.kind() == io::ErrorKind::TimedOut || e.kind() == io::ErrorKind::WouldBlock =>
             {
@@ -148,13 +153,44 @@ impl<'a, T: SetReadTimeout + Read + Write + 'a> Handle<'a, T> {
 
     /// Block until the selected mailbox changes, or until the given amount of time has expired.
     pub fn wait_timeout(mut self, timeout: Duration) -> Result<bool> {
+        self.old_timeout = self.session.stream.get_mut().read_timeout()?;
         self.session
             .stream
             .get_mut()
             .set_read_timeout(Some(timeout))?;
-        let res = self.wait_inner();
-        let _ = self.session.stream.get_mut().set_read_timeout(None).is_ok();
+        let res = self.wait_inner_keepalive();
         res
+    }
+
+    fn wait_inner_keepalive(&mut self) -> Result<bool> {
+        let mut v = Vec::new();
+        match self.session.readline(&mut v) {
+            Err(Error::Io(ref e))
+                if e.kind() == io::ErrorKind::TimedOut || e.kind() == io::ErrorKind::WouldBlock =>
+            {
+                if self.session.debug {
+                    eprintln!("wait_inner got error {:?}", e);
+                }
+                self.restore_timeout()?;
+                self.terminate()?;
+                Ok(false)
+            }
+            Err(err) => {
+                self.restore_timeout()?;
+                Err(err)
+            }
+            Ok(_) => {
+                self.restore_timeout()?;
+                Ok(true)
+            }
+        }
+    }
+
+    fn restore_timeout(&mut self) -> Result<()> {
+        self.session
+            .stream
+            .get_mut()
+            .set_read_timeout(self.old_timeout)
     }
 }
 
@@ -169,11 +205,18 @@ impl<'a> SetReadTimeout for TcpStream {
     fn set_read_timeout(&mut self, timeout: Option<Duration>) -> Result<()> {
         TcpStream::set_read_timeout(self, timeout).map_err(Error::Io)
     }
+
+    fn read_timeout(&self) -> Result<Option<Duration>> {
+        TcpStream::read_timeout(self).map_err(Error::Io)
+    }
 }
 
 #[cfg(feature = "tls")]
 impl<'a> SetReadTimeout for TlsStream<TcpStream> {
     fn set_read_timeout(&mut self, timeout: Option<Duration>) -> Result<()> {
         self.get_ref().set_read_timeout(timeout).map_err(Error::Io)
+    }
+    fn read_timeout(&self) -> Result<Option<Duration>> {
+        self.get_ref().read_timeout().map_err(Error::Io)
     }
 }
